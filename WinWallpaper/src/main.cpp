ca87a59ab3f3a12466bc4fe9f136df067fe3bafc
TecStylos <v.danninger04@gmail.com>
2021-04-07 20:13:01 +0200

    Added function to detect the position and dimension of every monitor. Moved EnumWorkerWProc to local lambda.

--- WinWallpaper/src/main.cpp
+++ WinWallpaper/src/main.cpp
@@ -1,63 +1,101 @@
 #include <iostream>
+#include <vector>
 #include <windows.h>
 
-BOOL CALLBACK EnumWorkerWProc(HWND hWnd, LPARAM lParam)
-{
-	HWND p = FindWindowEx(hWnd, NULL, L"SHELLDLL_DefView", NULL);
-	HWND* ret = (HWND*)lParam;
+struct Rect {
+	int x, y;
+	int w, h;
+};
 
-	if (p)
-	{
-		*ret = FindWindowEx(NULL, hWnd, L"WorkerW", NULL);
-	}
+std::vector<Rect> getMonitorMetrics() {
+	struct EnumInfo {
+		int scrLeft;
+		int scrTop;
+		std::vector<Rect> metrics;
+	};
 
-	return true;
+	EnumInfo enumInfo;
+	enumInfo.scrLeft = GetSystemMetrics(SM_XVIRTUALSCREEN);
+	enumInfo.scrTop = GetSystemMetrics(SM_YVIRTUALSCREEN);
+
+	auto enumProc = [](HMONITOR hMon, HDC hdc, LPRECT lpRect, LPARAM lParam) -> BOOL {
+		auto& enumInfo = *(EnumInfo*)lParam;
+
+		Rect rect;
+		rect.x = lpRect->left - enumInfo.scrLeft;
+		rect.y = lpRect->top - enumInfo.scrTop;
+		rect.w = lpRect->right - lpRect->left;
+		rect.h = lpRect->bottom - lpRect->top;
+
+		enumInfo.metrics.push_back(rect);
+
+		return true;
+	};
+
+	HDC hdc = GetDC(NULL);
+	EnumDisplayMonitors(hdc, NULL, enumProc, (LPARAM)&enumInfo);
+	ReleaseDC(NULL, hdc);
+
+	return enumInfo.metrics;
 }
+
 
 HWND getWallpaperHandle()
 {
 	HWND progman = FindWindow(L"ProgMan", NULL);
 	SendMessageTimeout(progman, 0x052C, 0, 0, SMTO_NORMAL, 1000, NULL);
+
 	HWND hWndWallpaper = NULL;
-	EnumWindows(EnumWorkerWProc, (LPARAM)&hWndWallpaper);
+
+	auto enumProc = [](HWND hWnd, LPARAM lParam) -> BOOL {
+		HWND p = FindWindowEx(hWnd, NULL, L"SHELLDLL_DefView", NULL);
+		HWND* ret = (HWND*)lParam;
+
+		if (p)
+		{
+			*ret = FindWindowEx(NULL, hWnd, L"WorkerW", NULL);
+		}
+
+		return true;
+	};
+
+	EnumWindows(enumProc, (LPARAM)&hWndWallpaper);
 	return hWndWallpaper;
 }
 
 int main(int argc, const char* argv[], const char* env[])
 {
 	HWND wallpaper = getWallpaperHandle();
-	RECT rectWallpaper;
-	GetWindowRect(wallpaper, &rectWallpaper);
-
-	int width = rectWallpaper.right - rectWallpaper.left;
-	int height = rectWallpaper.bottom - rectWallpaper.top;
-	std::cout << "Wallpaper dimensions: " << width << "x" << height << std::endl;
+	auto metrics = getMonitorMetrics();
 
 	HDC hdc = GetDC(wallpaper);
 
-	COLORREF* arr = new COLORREF[1024 * 1024];
+	for (size_t i = 0; i < metrics.size(); ++i) {
+		auto& rect = metrics[i];
 
-	for (int y = 0; y < 1024; ++y)
-	{
-		for (int x = 0; x < 1024; ++x)
-		{
-			int b = x % 256;
-			int g = y % 256;
-			int r = 0;
-			arr[x + y * 1024] = RGB(r, g, b);
+		COLORREF* arr = new COLORREF[rect.w * rect.h];
+
+		for (int y = 0; y < rect.h; ++y) {
+			for (int x = 0; x < rect.w; ++x) {
+				int b = x * 255 / rect.w;
+				int g = y * 255 / rect.h;
+				int r = (i + 1) * 255 / metrics.size();
+				r = 0;
+				arr[x + y * rect.w] = RGB(r, g, b);
+			}
 		}
+
+		HBITMAP map = CreateBitmap(rect.w, rect.h, 1, 8 * 4, arr);
+
+		HDC src = CreateCompatibleDC(hdc);
+		SelectObject(src, map);
+		BitBlt(hdc, rect.x, rect.y, rect.w, rect.h, src, 0, 0, SRCCOPY);
+
+		DeleteObject(map);
+		DeleteDC(src);
+
+		delete[] arr;
 	}
-
-	HBITMAP map = CreateBitmap(1024, 1024, 1, 8 * 4, arr);
-
-	HDC src = CreateCompatibleDC(hdc);
-	SelectObject(src, map);
-	BitBlt(hdc, -rectWallpaper.left, -rectWallpaper.top, 1024, 1024, src, 0, 0, SRCCOPY);
-
-	DeleteObject(map);
-	DeleteDC(src);
-
-	delete[] arr;
 
 	ReleaseDC(wallpaper, hdc);
 
